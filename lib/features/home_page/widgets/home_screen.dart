e9f48915f63425a672051a8c8e86002dd6f5cbfc
asmaa <elsaeeedasmaa@gmail.com>
2024-12-25 21:53:15 +0200

    creating bottom navigation bar

--- lib/features/home_page/widgets/home_screen.dart
+++ lib/features/home_page/widgets/home_screen.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:store_app/core/themes/styles.dart';
 import 'package:store_app/features/products_screen/products_screen.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const ProductsScreen();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('New Trends', style:Styles.textStyle45,),
+        actions: [
+          IconButton(onPressed: (){},
+              icon: const Icon(Icons.shopping_cart_sharp)
+          ),
+        ],
+      ),
+      body: const ProductsScreen(),
+    );
   }
 }
--- lib/features/products_screen/widgets/products_grid_list.dart
+++ lib/features/products_screen/widgets/products_grid_list.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'custom_stack.dart';
 class ProductsGridList extends StatelessWidget {
   const ProductsGridList({
